a896e5d5599e1ce0523727862394866294766e44
gst <911784100@qq.com>
2022-08-31 11:47:07 +0800

    第2次提交

--- src/main/java/com/atguigu/git/Hello.java
+++ src/main/java/com/atguigu/git/Hello.java
@@ -4,5 +4,7 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("2222222");
+        System.out.println("2");
+        System.out.println("2");
     }
 }
